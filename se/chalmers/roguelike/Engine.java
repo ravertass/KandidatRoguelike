0ad63bf0659ba395f9bad7ecf86c998b14ab835a
ravertass <ravertass@gmail.com>
2014-02-11 16:44:35 +0100

    Added some graphics stuff

--- se/chalmers/roguelike/Engine.java
+++ se/chalmers/roguelike/Engine.java
@@ -1,5 +1,7 @@
 package se.chalmers.roguelike;
 
+import se.chalmers.roguelike.Systems.*;
+
 public class Engine {
 
 	/**
@@ -8,6 +10,11 @@ public class Engine {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 
+		Rendering renderingSystem = new Rendering();
+		for (int i = 0; i < 10000; i++) {
+			renderingSystem.update();
+		}
+		renderingSystem.exit();
 	}
 
 }
