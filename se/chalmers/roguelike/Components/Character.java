81eb418854e7d87f5ea5d0e6c5bf7bb755dec6ba
nebin <samuel.tiensuu@gmail.com>
2014-02-11 19:07:02 +0100

    Added Character.java

---
+++ se/chalmers/roguelike/Components/Character.java
@@ -0,0 +1,27 @@
+package se.chalmers.roguelike.Components;
+
+public class Character {
+	private String name;
+	private int level;
+	
+	public Character (String name, int level){
+		this.name = name;
+		this.level = level;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public int getLevel(){
+		return level;
+	}
+	
+	public void setLevel(int level){
+		this.level = level;
+	}
+}
