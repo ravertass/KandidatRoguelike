f5ac27a1af0fd7af0ca979360b3cc61153345a6f
twister <simonwalmgren@gmail.com>
2014-02-15 14:36:38 +0100

    fixed conflicts

--- se/chalmers/roguelike/EntityCreator.java
+++ se/chalmers/roguelike/EntityCreator.java
@@ -2,6 +2,7 @@ package se.chalmers.roguelike;
 
 import se.chalmers.roguelike.Components.Direction;
 import se.chalmers.roguelike.Components.Health;
+import se.chalmers.roguelike.Components.Highlight;
 import se.chalmers.roguelike.Components.Input;
 import se.chalmers.roguelike.Components.Position;
 import se.chalmers.roguelike.Components.Sprite;
@@ -15,19 +16,29 @@ public class EntityCreator {
 	
 	public EntityCreator(Engine engine){
 		this.engine = engine;
+		this.input = new Input();
 	}
 	
 	public void createPlayer(){
 		Entity player = new Entity();
 		player.add(new Health(100));
 		player.add(new TurnsLeft(1));
-		player.add(new Input());
+		player.add(input);
 		player.add(new Sprite("player"));
 		player.add(new Position(10,10));
 		player.add(new Direction());
 		//engine.addToInputSys(player);
 		//engine.addToRenderingSys(player); // depreached, left to show
 		engine.addEntity(player); // should this be used? possibly just pass it to system directly
+	}
+	
+	public void createHighlight() {
+		Entity highlight = new Entity();
+		highlight.add(new Sprite("highlight"));
+		highlight.add(new Position(10,10));
+		highlight.add(input);
+		highlight.add(new Highlight());
+		engine.addEntity(highlight);
 	}
 
 }
