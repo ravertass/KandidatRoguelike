d42ed5b51062c93980d549561f9abc8a726c81dd
ravertass <ravertass@gmail.com>
2014-02-15 19:27:24 +0100

    Forgot to 'git add' a moved class. :)

---
+++ se/chalmers/roguelike/Entity.java
@@ -0,0 +1,63 @@
+package se.chalmers.roguelike;
+
+import java.util.HashMap;
+
+import se.chalmers.roguelike.Components.*;
+import se.chalmers.roguelike.Components.Character;
+
+/**
+ * Entity class 
+ */
+public class Entity {
+
+	HashMap<Class<?>, IComponent> components;
+	private int componentKey;
+	
+	public Entity(){
+		components = new HashMap<Class<?>, IComponent>();
+		componentKey = 0;
+	}
+	
+	public void add(IComponent component) {
+		Class<?> compClass = component.getClass();
+		components.put(compClass, component);
+		
+		// Determine the new component key for the entity.
+		if(compClass == Character.class){
+			componentKey |= Engine.CompCharacter;
+		} else if(compClass == Health.class){
+			componentKey |= Engine.CompHealth;
+		} else if(compClass == Input.class){
+			componentKey |= Engine.CompInput;
+		} else if(compClass == Position.class){
+			componentKey |= Engine.CompPosition;
+		} else if(compClass == Sprite.class){
+			componentKey |= Engine.CompSprite;
+		} else if(compClass == TurnsLeft.class){
+			componentKey |= Engine.CompTurnsLeft;
+		} else if(compClass == Direction.class){
+			componentKey |= Engine.CompDirection;
+		} else if(compClass == AI.class){
+			componentKey |= Engine.CompAI;
+		} else if(compClass == Highlight.class){
+			componentKey |= Engine.CompHighlight;
+		} else if(compClass == Player.class){
+			componentKey |= Engine.CompPlayer;
+		}
+		System.out.println("New compkey: "+componentKey); // debug
+	}
+	
+	public int getComponentKey(){
+		return componentKey;
+	}
+	
+	public void remove(IComponent component) {
+		components.remove(component.getClass());
+	}
+	
+	public <T extends IComponent> T getComponent(Class<T> type) {
+		
+		return type.cast(components.get(type));
+	}
+	
+}
