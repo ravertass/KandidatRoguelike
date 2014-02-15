7328296bc0ea5532a4a1d957af4c5098f08b94bd
Leo Anttila <anttila@modu.se>
2014-02-15 15:12:02 +0100

    Changed input system to only work on player

--- se/chalmers/roguelike/Systems/InputSystem.java
+++ se/chalmers/roguelike/Systems/InputSystem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.lwjgl.input.*;
 
+import se.chalmers.roguelike.Engine;
 import se.chalmers.roguelike.Components.Input;
 import se.chalmers.roguelike.Entities.Entity;
 import se.chalmers.roguelike.util.Pair;
@@ -16,27 +17,27 @@ public class InputSystem implements ISystem {
 	 * The effected entities.
 	 */
 	private ArrayList<Entity> entities;
+	private Entity player;
 	
 	public InputSystem(){
 		entities = new ArrayList<Entity>();
 	}
 		
 	public void update() {
+		if(player == null){
+			return;
+		}
 		while(Keyboard.next()){
 			if(Keyboard.getEventKeyState()){
 				if (Keyboard.getEventKey() == Keyboard.KEY_ESCAPE)
 					System.exit(0);
-				for (Entity e : entities) {
-					e.getComponent(Input.class).setNextKey(Keyboard.getEventKey());
-				}
+				player.getComponent(Input.class).setNextKey(Keyboard.getEventKey());
 			}
 		}
 		while(Mouse.next()) {
 			if(Mouse.getEventButtonState()) {
-				for (Entity e : entities) {
-					e.getComponent(Input.class).setNextMouseClick(
-							new Pair<Integer,Integer>(Mouse.getX(),Mouse.getY()));
-				}
+				player.getComponent(Input.class).setNextMouseClick(
+						new Pair<Integer,Integer>(Mouse.getX(),Mouse.getY()));
 			}
 			
 		}
@@ -46,7 +47,11 @@ public class InputSystem implements ISystem {
 	 * @param e
 	 */
 	public void addEntity(Entity e) {
-		entities.add(e);
+		if((e.getComponentKey() & Engine.CompPlayer) == Engine.CompPlayer){
+			player = e;
+		} else {
+			entities.add(e);
+		}
 	}
 	/**
 	 * Removes and effected entity from the list of effected entities.
