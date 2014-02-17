ca15d5b4e5807267aaddb0f743eca77387035a6d
Leo Anttila <anttila@modu.se>
2014-02-17 17:51:29 +0100

    Fixed some javadoc for the class and added methods to change contents of the world

--- se/chalmers/roguelike/World/World.java
+++ se/chalmers/roguelike/World/World.java
@@ -3,12 +3,36 @@ package se.chalmers.roguelike.World;
 import se.chalmers.roguelike.Components.Sprite;
 import se.chalmers.roguelike.util.Dice;
 
+/**
+ * World is an class that holds the game world along with several helper 
+ * functions for determining if the world can be walked on etc.
+ * 
+ * @author Anttila
+ *
+ */
 public class World {
 	
-	int worldWidth;
-	int worldHeight;
-	Tile[][] tiles;
+	private int worldWidth;
+	private int worldHeight;
+	private Tile[][] tiles;
 	
+	/**
+	 * Creates a new world object
+	 * 
+	 * @param worldWidth the width of the world
+	 * @param worldHeight the height of the world
+	 * @param tiles the array of tiles the world should have
+	 */
+	public World(int worldWidth, int worldHeight, Tile[][] tiles){
+		this.worldWidth = worldWidth;
+		this.worldHeight = worldHeight;
+		this.tiles = tiles;
+	}
+	
+	/**
+	 * Currently generates a new world, will probably be removed in the future
+	 * when a real level generator exists
+	 */
 	public World(){
 		
 		// Debug world
@@ -34,11 +58,25 @@ public class World {
 		}
 	}
 	
+	/**
+	 * Looks up if something can walk on a tile
+	 * 
+	 * @param x x coordinate for the tile to look up
+	 * @param y y coordinate for the tile to look up
+	 * @return true if the tile can be walked on, false if it can not (already occupied, walls, etc)
+	 */
 	public boolean isWalkable(int x, int y){
 		return x >= 0 && x < worldWidth && y >=0 && y < worldHeight && 
 				tiles[x][y].isWalkable() && tiles[x][y] != null;
 	}
 	
+	/**
+	 * Looks up and returns a tile 
+	 * 
+	 * @param x x coordinate for the tile to look up
+	 * @param y y coordinate for the tile to look up
+	 * @return returns the tile if it exists, otherwise null
+	 */
 	public Tile getTile(int x, int y){
 		if(x < 0 || x >= worldHeight || y < 0 || y >= worldHeight){
 			return null;
@@ -52,5 +90,21 @@ public class World {
 	
 	public int getWorldHeight(){
 		return worldHeight;
+	}
+	
+	public Tile[][] getWorld(){
+		return tiles;
+	}
+	
+	/**
+	 * Method use to changing the world
+	 * @param worldWidth the width of the new world, must match the array
+	 * @param worldHeight the height of the new world, must match the array
+	 * @param tiles a 2d-array of all the tiles for the world
+	 */
+	public void setWorld(int worldWidth, int worldHeight, Tile[][] tiles){
+		this.worldWidth = worldWidth;
+		this.worldHeight = worldHeight;
+		this.tiles = tiles;
 	}
 }
