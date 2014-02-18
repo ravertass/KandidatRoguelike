597a2f16ca969fe16664432c905e66fc4d9ab3f9
twister <simonwalmgren@gmail.com>
2014-02-18 19:33:23 +0100

    added a system for handling input directed to the player

---
+++ se/chalmers/roguelike/Systems/PlayerInputSystem.java
@@ -0,0 +1,38 @@
+package se.chalmers.roguelike.Systems;
+
+import se.chalmers.roguelike.Entity;
+import se.chalmers.roguelike.util.Observer;
+
+public class PlayerInputSystem implements ISystem, Observer {
+	
+	Entity player;
+	
+	public PlayerInputSystem() {
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notify(Enum<?> i) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
