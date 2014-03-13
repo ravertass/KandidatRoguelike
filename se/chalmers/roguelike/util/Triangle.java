7a9c66302bdaab0bd6fe931abc97986916150434
nebin <samuel.tiensuu@gmail.com>
2014-03-13 11:54:41 +0100

    Added Triangle

---
+++ se/chalmers/roguelike/util/Triangle.java
@@ -0,0 +1,20 @@
+package se.chalmers.roguelike.util;
+
+public class Triangle {
+
+	private int x1;
+	private int y1;
+	private int x2;
+	private int y2;
+	private int x3;
+	private int y3;
+
+	public Triangle (int x1, int y1, int x2, int y2, int x3,int y3) {
+		this.x1 = x1;
+		this.y1 = y1;
+		this.x2 = x2;
+		this.y2 = y2;
+		this.x3 = x3;
+		this.y3 = y3;
+	}
+}
