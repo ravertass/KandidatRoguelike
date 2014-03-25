fea85facfc6fea9f26b39c4a3c1a7b3f7344352f
unknown <simonwalmgren@gmail.com>
2014-03-25 21:12:52 +0100

    added a few lines and some javadoc

--- se/chalmers/roguelike/Systems/InventorySystem.java
+++ se/chalmers/roguelike/Systems/InventorySystem.java
@@ -11,46 +11,62 @@ import se.chalmers.roguelike.World.Dungeon;
 import se.chalmers.roguelike.World.Tile;
 import se.chalmers.roguelike.util.Observer;
 
+/**
+ * This system handles all the enteties which have an inventory, the player and
+ * all the mobs.
+ * 
+ * @author twister
+ * 
+ */
 public class InventorySystem implements ISystem, Observer {
-	
+
 	private ArrayList<Entity> entities;
 	ArrayList<Entity> toRemove;
-	
+
 	private Entity player;
-	
+
 	private Dungeon world;
-	
+
 	private boolean timeToLoot;
-	
+
 	public InventorySystem() {
 		this.entities = new ArrayList<Entity>();
 		toRemove = new ArrayList<Entity>();
 		timeToLoot = false;
 	}
-	
+
 	@Override
 	public void update() {
 
 	}
-	
+
+	/**
+	 * This will check if the g button has been pressed and then will pick upp
+	 * all pocketable items that is on the tile.
+	 * 
+	 * @param d
+	 */
 	public void update(Dungeon d) {
 		this.world = d;
-		if(timeToLoot) {
+		if (timeToLoot) {
 			Position p = player.getComponent(Position.class);
 			Tile playerTile = world.getTile(p.getX(), p.getY());
 			toRemove.clear();
-			for(Entity e : playerTile.getEntities()) {
-				if((e.getComponentKey() & Engine.CompPocketable) == Engine.CompPocketable && !player.getComponent(Inventory.class).isFull()) {
+			for (Entity e : playerTile.getEntities()) {
+				if ((e.getComponentKey() & Engine.CompPocketable) == Engine.CompPocketable
+						&& !player.getComponent(Inventory.class).isFull()) {
 					player.getComponent(Inventory.class).addItem(e);
 					toRemove.add(e);
 				}
 			}
-			for(Entity e : toRemove) {
-				
-				playerTile.removeEntity(e); //removes the picked up items from the current tile
+			for (Entity e : toRemove) {
+
+				playerTile.removeEntity(e); // removes the picked up items from
+											// the current tile
 			}
+			timeToLoot = false;
 		}
-		
+
 	}
 
 	@Override
@@ -59,21 +75,19 @@ public class InventorySystem implements ISystem, Observer {
 			this.player = entity;
 		}
 		this.entities.add(entity);
-		
+
 	}
 
 	@Override
 	public void removeEntity(Entity entity) {
 		this.entities.add(entity);
-		
+
 	}
 
 	@Override
 	public void notify(Enum<?> i) {
-		if((InputAction)i == InputAction.LOOT)
+		if ((InputAction) i == InputAction.LOOT)
 			timeToLoot = true;
 	}
-
-
 
 }
