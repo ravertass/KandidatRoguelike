4f0ce8718104b55344ae911f17f35f21b0a67fea
Leo Anttila <anttila@modu.se>
2014-03-25 11:46:11 +0100

    Added an interaction system basis

---
+++ se/chalmers/roguelike/Systems/InteractionSystem.java
@@ -0,0 +1,25 @@
+package se.chalmers.roguelike.Systems;
+
+import se.chalmers.roguelike.Entity;
+
+public class InteractionSystem implements ISystem {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
