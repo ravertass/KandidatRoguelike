b6ff1a7c41139527b166f7ae12b97e127ef34532
twister <simonwalmgren@gmail.com>
2014-04-15 11:52:21 +0200

    started working on statuseffects

---
+++ se/chalmers/roguelike/Systems/StatusEffectSystem.java
@@ -0,0 +1,33 @@
+package se.chalmers.roguelike.Systems;
+
+import se.chalmers.roguelike.Entity;
+
+public class StatusEffectSystem implements ISystem {
+	
+	public static enum StatusEffect {
+		
+	}
+	
+	public StatusEffectSystem() {
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeEntity(Entity entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
