c26721001cfb95667b1d48c60bd6e7ed945d41a9
twister <simonwalmgren@gmail.com>
2014-04-18 11:18:04 +0200

    the effects will now affect the entity affected by the statuseffect

--- se/chalmers/roguelike/Systems/StatusEffectSystem.java
+++ se/chalmers/roguelike/Systems/StatusEffectSystem.java
@@ -4,58 +4,81 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import se.chalmers.roguelike.Entity;
+import se.chalmers.roguelike.Components.Health;
 import se.chalmers.roguelike.Components.StatusEffects;
+import se.chalmers.roguelike.Components.TurnsLeft;
 import se.chalmers.roguelike.util.Observer;
 
 public class StatusEffectSystem implements ISystem, Observer {
-	
+
 	public ArrayList<Entity> entities;
-	
+
 	public boolean newTurn = false;
-	
+
 	public static enum StatusEffect {
-		POISONED, SLOWED, BURNING, PARALYZED
+		POISONED, BURNING, PARALYZED
 	}
-	
+
 	public StatusEffectSystem() {
 		entities = new ArrayList<Entity>();
 	}
 
 	@Override
 	public void update() {
-		if(newTurn) {
+		if (newTurn) {
+			takeEffects();
 			decreaseTurnsLeft();
 			newTurn = false;
 		}
-		
+
 	}
-	
+
 	@Override
 	public void addEntity(Entity entity) {
 		entities.add(entity);
-		
+
 	}
 
 	@Override
 	public void removeEntity(Entity entity) {
 		entities.remove(entity);
-		
+
 	}
+
 	/**
 	 * This will be called by turnsystem whenever there is a new turn.
 	 */
 	@Override
 	public void notify(Enum<?> i) {
 		newTurn = true;
-		
+
 	}
-	
-	private void decreaseTurnsLeft() {
-		for(Entity e : entities) {
-			StatusEffects ses = e.getComponent(StatusEffects.class);
-			HashMap<StatusEffect, Integer> effects = ses.getEffects();
+
+	private void takeEffects() {
+		for (Entity e : entities) {
+			HashMap<StatusEffect, Integer> effects = e.getComponent(StatusEffects.class).getEffects();
 			for(StatusEffect se : effects.keySet()) {
-				if(effects.get(se) == 1) {
+				switch(se) {
+					case BURNING: 
+						e.getComponent(Health.class).decreaseHealth(5); //TODO magic number
+						break;
+					case POISONED:
+						e.getComponent(Health.class).decreaseHealth(5); //TODO moar magic numberz
+						break;
+					case PARALYZED:
+						e.getComponent(TurnsLeft.class).decreaseTurnsLeft();
+						break;
+												
+				}
+			}
+		}
+	}
+
+	private void decreaseTurnsLeft() {
+		for (Entity e : entities) {
+			HashMap<StatusEffect, Integer> effects = e.getComponent(StatusEffects.class).getEffects();
+			for (StatusEffect se : effects.keySet()) {
+				if (effects.get(se) == 1) {
 					effects.remove(se);
 				} else {
 					effects.put(se, effects.get(se) - 1);
