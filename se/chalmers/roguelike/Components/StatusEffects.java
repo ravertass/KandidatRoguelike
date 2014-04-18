93546d340cc43b5c87b4cb2632e80051c8b425bc
twister <simonwalmgren@gmail.com>
2014-04-18 10:50:24 +0200

    updated turnsystem so now other systems can observe it and know when there is a new turn

--- se/chalmers/roguelike/Components/StatusEffects.java
+++ se/chalmers/roguelike/Components/StatusEffects.java
@@ -1,19 +1,24 @@
 package se.chalmers.roguelike.Components;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import se.chalmers.roguelike.Systems.StatusEffectSystem.StatusEffect;
 
-public class StatusEffects {
+public class StatusEffects implements IComponent {
 	
-	ArrayList<StatusEffect> statusEffects;
+	HashMap<StatusEffect, Integer> statusEffects;
 	
 	public StatusEffects() {
-		statusEffects = new ArrayList<StatusEffect>();
+		statusEffects = new HashMap<StatusEffect, Integer>();
 	}
 	
-	public void addEffect(StatusEffect se) {
-		this.statusEffects.add(se);
+	public HashMap<StatusEffect, Integer> getEffects() {
+		return this.statusEffects;
+	}
+	
+	public void addEffect(StatusEffect se, int turnsLeft) {
+		this.statusEffects.put(se,turnsLeft);
 	}
 	
 	public void removeEffect(StatusEffect se) {
@@ -22,6 +27,16 @@ public class StatusEffects {
 	
 	public void removeAllEffects() {
 		this.statusEffects.clear();
+	}
+	
+	public void setStatusEffects(HashMap<StatusEffect, Integer> h) {
+		this.statusEffects = h;
+	}
+	
+	public StatusEffects clone() {
+		StatusEffects se = new StatusEffects();
+		se.setStatusEffects(new HashMap<StatusEffect, Integer>(statusEffects));
+		return se;
 	}
 
 }
--- se/chalmers/roguelike/Systems/StatusEffectSystem.java
+++ se/chalmers/roguelike/Systems/StatusEffectSystem.java
@@ -1,12 +1,17 @@
 package se.chalmers.roguelike.Systems;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import se.chalmers.roguelike.Entity;
+import se.chalmers.roguelike.Components.StatusEffects;
+import se.chalmers.roguelike.util.Observer;
 
-public class StatusEffectSystem implements ISystem {
+public class StatusEffectSystem implements ISystem, Observer {
 	
 	public ArrayList<Entity> entities;
+	
+	public boolean newTurn = false;
 	
 	public static enum StatusEffect {
 		POISONED, SLOWED, BURNING, PARALYZED
@@ -18,20 +23,45 @@ public class StatusEffectSystem implements ISystem {
 
 	@Override
 	public void update() {
-		// TODO Auto-generated method stub
+		if(newTurn) {
+			decreaseTurnsLeft();
+			newTurn = false;
+		}
 		
 	}
-
+	
 	@Override
 	public void addEntity(Entity entity) {
-		// TODO Auto-generated method stub
+		entities.add(entity);
 		
 	}
 
 	@Override
 	public void removeEntity(Entity entity) {
-		// TODO Auto-generated method stub
+		entities.remove(entity);
 		
+	}
+	/**
+	 * This will be called by turnsystem whenever there is a new turn.
+	 */
+	@Override
+	public void notify(Enum<?> i) {
+		newTurn = true;
+		
+	}
+	
+	private void decreaseTurnsLeft() {
+		for(Entity e : entities) {
+			StatusEffects ses = e.getComponent(StatusEffects.class);
+			HashMap<StatusEffect, Integer> effects = ses.getEffects();
+			for(StatusEffect se : effects.keySet()) {
+				if(effects.get(se) == 1) {
+					effects.remove(se);
+				} else {
+					effects.put(se, effects.get(se) - 1);
+				}
+			}
+		}
 	}
 
 }
